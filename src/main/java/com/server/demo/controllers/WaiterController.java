f4ac7dff5f6a3f0bc6caab29e58460c08f2559d8
Jekkan322 <eemelyanov-2000@mail.ru>
2022-01-09 23:25:11 +0500

    cross origin

--- src/main/java/com/server/demo/controllers/WaiterController.java
+++ src/main/java/com/server/demo/controllers/WaiterController.java
@@ -9,6 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class WaiterController {
 
     @Autowired
@@ -23,7 +24,6 @@ public class WaiterController {
         }
     }
 
-    @CrossOrigin(origins = "https://waitersproject.herokuapp.com")
     @GetMapping("/waiters/all")
     //@PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity getAllWaiters(){
